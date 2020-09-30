0e19415dd483003fb736f0649680f1fbcd2c45d7
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-09-30 14:30:22 +0100

    first commit; file setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# CSS Functions
+
+## Concepts to master:
+
+### Context:
+
+- New to css
+- Allow us some of the power that css preprocessors
+
+### Syntax
+
+- --some-variable: value;
+- some-property: var(--some-variable);
+
+## Your tasks to test your mastery of these:
+
+See [styles.css](styles.css) for tasks!
---
+++ main.test.js
@@ -0,0 +1,56 @@
+const { JSDOM } = require('jsdom');
+const {
+  toBeInTheDocument,
+  toBeEmptyDOMElement,
+  toHaveStyle,
+  toHaveAttribute,
+  toHaveClass,
+} = require('@testing-library/jest-dom/matchers');
+
+expect.extend({
+  toBeInTheDocument,
+  toBeEmptyDOMElement,
+  toHaveStyle,
+  toHaveAttribute,
+  toHaveClass,
+});
+
+const LEVELS = {
+  one: 'level_one',
+  two: 'level_two',
+  three: 'level_three',
+};
+
+let document = null;
+let window = null;
+beforeEach(async () => {
+  const jsDOM = await JSDOM.fromFile('index.html', {
+    runScripts: 'dangerously',
+    resources: 'usable',
+    url: `file://${__dirname}/`,
+    features: {
+      FetchExternalResources: ['script'],
+      ProcessExternalResources: ['script'],
+    },
+  });
+  await new Promise((res) => {
+    jsDOM.window.onload = res;
+  });
+  document = jsDOM.window.document;
+  window = jsDOM.window;
+});
+
+afterEach(() => {
+  document = null;
+  window = null;
+});
+
+describe(LEVELS.one, () => {
+  it('should have some property...', () => {});
+});
+describe(LEVELS.two, () => {
+  it('should have some property...', () => {});
+});
+describe(LEVELS.three, () => {
+  it('should have some property...', () => {});
+});
