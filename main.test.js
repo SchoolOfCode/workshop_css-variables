54dc5273a17cd289a801472cc40641a2513d472b
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-11-09 16:13:32 +0000

    tests written and passable and failable for lvl1

--- main.test.js
+++ main.test.js
@@ -6,6 +6,9 @@ const {
   toHaveAttribute,
   toHaveClass,
 } = require('@testing-library/jest-dom/matchers');
+const fs = require('fs');
+const util = require('util');
+const { read } = require('fs');
 
 expect.extend({
   toBeInTheDocument,
@@ -21,8 +24,15 @@ const LEVELS = {
   three: 'level_three',
 };
 
+function readModuleFile(path) {
+  var filename = require.resolve(path);
+  const readFile = util.promisify(fs.readFile);
+  return readFile(filename, 'utf8');
+}
+
 let document = null;
 let window = null;
+
 beforeEach(async () => {
   const jsDOM = await JSDOM.fromFile('index.html', {
     runScripts: 'dangerously',
@@ -33,9 +43,11 @@ beforeEach(async () => {
       ProcessExternalResources: ['script'],
     },
   });
+
   await new Promise((res) => {
     jsDOM.window.onload = res;
   });
+
   document = jsDOM.window.document;
   window = jsDOM.window;
 });
@@ -46,10 +58,60 @@ afterEach(() => {
 });
 
 describe(LEVELS.one, () => {
-  it.todo(
-    'should contain the correct CSS variables and values at the root level of the CSS'
-  ); //can put the variable names and values in arrays and do the ollll' .forEach so I don't have to write an individual test for each!
-  it.todo('should use the CSS variables in the correct places in the CSS'); //again, can just .forEach over some arrays to cut down on repetition
+  it.only('should contain the correct CSS variables and values at the root level of the CSS', async () => {
+    const cssAsString = await readModuleFile('./styles.css');
+    const rootCss = cssAsString.split(':root')[1].split('}')[0];
+    const expected = [
+      '--primary-colour: rgb(197, 231, 147)',
+      '--secondary-colour: antiquewhite',
+      '--text-colour: black',
+      '--header-size: 24px',
+      '--main-text-size: 18px',
+      '--border-radius: 10px',
+    ];
+    expected.forEach((cssVar) => {
+      expect(rootCss).toContain(cssVar);
+    });
+  });
+  it('should use the CSS variables in the correct places in the CSS - main', () => {
+    const { style } = Array.from(document.styleSheets[0].cssRules).find(
+      ({ selectorText }) => selectorText === 'main'
+    );
+    expect(style[`font-size`] == `var(--main-text-size)`).toBeTruthy();
+  });
+  it('should use the CSS variables in the correct places in the CSS - body', () => {
+    const { style } = Array.from(document.styleSheets[0].cssRules).find(
+      ({ selectorText }) => selectorText === 'body'
+    );
+    expect(style[`color`] == `var(--text-colour)`).toBeTruthy();
+    expect(style[`background-color`] == `var(--secondary-colour)`).toBeTruthy();
+  });
+  it('should use the CSS variables in the correct places in the CSS - button', () => {
+    const { style } = Array.from(document.styleSheets[0].cssRules).find(
+      ({ selectorText }) => selectorText === 'button'
+    );
+    expect(style[`font-size`] == `var(--main-text-size)`).toBeTruthy();
+    expect(style[`border-radius`] == `var(--border-radius)`).toBeTruthy();
+  });
+  it('should use the CSS variables in the correct places in the CSS - .plant-header', () => {
+    const { style } = Array.from(document.styleSheets[0].cssRules).find(
+      ({ selectorText }) => selectorText === '.plant-header'
+    );
+    expect(style[`font-size`] == `var(--header-size)`).toBeTruthy();
+  });
+  it('should use the CSS variables in the correct places in the CSS - .plant-listing', () => {
+    const { style } = Array.from(document.styleSheets[0].cssRules).find(
+      ({ selectorText }) => selectorText === '.plant-listing'
+    );
+    expect(style[`background-color`] == `var(--primary-colour)`).toBeTruthy();
+    expect(style[`border-radius`] == `var(--border-radius)`).toBeTruthy();
+  });
+  it('should use the CSS variables in the correct places in the CSS - .plant-pic', () => {
+    const { style } = Array.from(document.styleSheets[0].cssRules).find(
+      ({ selectorText }) => selectorText === '.plant-pic'
+    );
+    expect(style[`border-radius`] == `var(--border-radius)`).toBeTruthy();
+  });
 });
 describe(LEVELS.two, () => {
   it.todo(
