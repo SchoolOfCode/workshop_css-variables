38adce36bde101f63d56f40524c9a6e321363087
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-11-11 14:53:42 +0000

    blood, sweat, and tears of liz and tim have gone into making the click work in the last test without rewire

--- main.test.js
+++ main.test.js
@@ -6,10 +6,8 @@ const {
   toHaveAttribute,
   toHaveClass,
 } = require('@testing-library/jest-dom/matchers');
-const fs = require('fs');
-const util = require('util');
-const { read } = require('fs');
-const rewire = require('rewire');
+
+const { fireEvent } = require('@testing-library/dom');
 
 expect.extend({
   toBeInTheDocument,
@@ -24,12 +22,6 @@ const LEVELS = {
   two: 'level_two',
   three: 'level_three',
 };
-
-function readModuleFile(path) {
-  var filename = require.resolve(path);
-  const readFile = util.promisify(fs.readFile);
-  return readFile(filename, 'utf8');
-}
 
 let document = null;
 let window = null;
@@ -60,18 +52,25 @@ afterEach(() => {
 
 describe(LEVELS.one, () => {
   it('should contain the correct CSS variables and values at the root level of the CSS', async () => {
-    const cssAsString = await readModuleFile('./styles.css');
-    const rootCss = cssAsString.split(':root')[1].split('}')[0];
-    const expected = [
-      '--primary-colour: rgb(197, 231, 147)',
-      '--secondary-colour: antiquewhite',
-      '--text-colour: black',
-      '--header-size: 24px',
-      '--main-text-size',
-      '--border-radius: 10px',
+    const variables = [
+      '--primary-colour',
+      '--secondary-colour',
+      '--text-colour',
+      '--header-size',
+      '--border-radius',
     ];
-    expected.forEach((cssVar) => {
-      expect(rootCss).toContain(cssVar);
+    const values = [
+      'rgb(197, 231, 147)',
+      'antiquewhite',
+      'black',
+      '24px',
+      '10px',
+    ];
+    const { style } = Array.from(document.styleSheets[0].cssRules).find(
+      ({ selectorText }) => selectorText === ':root'
+    );
+    variables.forEach((variable, i) => {
+      expect(style[variable] == values[i]).toBeTruthy();
     });
   });
   it('should use the CSS variables in the correct places in the CSS - main', () => {
@@ -116,16 +115,17 @@ describe(LEVELS.one, () => {
 });
 
 describe(LEVELS.two, () => {
-  it(`should have the value of main-text-size be 18px (without changing any of the places where it's used to 18px in the rest of the CSS)`, async () => {
+  it(`should not change font-size in main css`, async () => {
     const { style } = Array.from(document.styleSheets[0].cssRules).find(
       ({ selectorText }) => selectorText === 'main'
     );
     expect(style[`font-size`] == `18px`).not.toBeTruthy();
-    const cssAsString = await readModuleFile('./styles.css');
-    const rootCss = cssAsString.split(':root')[1].split('}')[0];
-    const expected = '--main-text-size: 18px';
-    expect(rootCss).toContain(expected);
-    expect(cssAsString).not.toContain('font-size: 18px');
+  });
+  it(`should have the value of main-text-size be 18px`, async () => {
+    const { style } = Array.from(document.styleSheets[0].cssRules).find(
+      ({ selectorText }) => selectorText === ':root'
+    );
+    expect(style['--main-text-size']).toBe('18px');
   });
 });
 
@@ -139,12 +139,11 @@ describe(LEVELS.three, () => {
     expect(style['--text-colour']).toBe('white');
   });
   it('should have function called toggleDarkMode in the JS that toggles the .dark-mode-theme class on and off of the body', () => {
-    //FIXME: Not sure why rewire is breaking here! What I want to do is use rewire to hand in the JSDOM version of document so that toggleDarkMode can act on it, like I've done in JS tests previously.
-    // const main = rewire('./main.js');
-    // main.__set__('document', document);
-    // main.__get__('toggleDarkMode')();
-    const actual = window.toggleDarkMode.toString();
-    expect(actual).toContain('body.classList.toggle');
-    expect(actual).toContain('dark-mode-theme');
+    const darkModeButton = document.querySelector('#dark-mode-button');
+
+    fireEvent.click(darkModeButton);
+
+    const body = document.querySelector('body');
+    expect(body).toHaveClass('dark-mode-theme');
   });
 });
